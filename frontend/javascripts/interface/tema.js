a9aa9e56d98b5da428d0cf8457d3758008f04b90
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-10-24 10:29:28 -0300

    chore: criação do elemento DOM de botão tema, adição do evento listener e teste usando a função alterarTema().

--- frontend/javascripts/interface/tema.js
+++ frontend/javascripts/interface/tema.js
@@ -4,3 +4,14 @@
  * Este script é responsável por alterar o tema do site entre claro e escuro.
  */
 
+// Obter botão de menu pelo DOM
+const botaoTema = document.getElementById("botao-tema")
+// Adicionar evento listener ao clicar no botão de tema
+botaoTema.addEventListener("click", alterarTema)
+
+/**
+ * Altera o valor do tema atual (light ou dark).
+ */
+function alterarTema() {
+  window.alert("Botão clicado!")
+}
