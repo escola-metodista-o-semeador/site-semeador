935d8e334ee2a7c91c277ff9d27b15cde4447827
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-10-25 08:16:38 -0300

    feat: tema claro e escuro.
    commit ab19183f986b1746310cb9c3285746b8116fed68
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Fri Oct 25 08:15:48 2024 -0300
    
        chore(theme): efeito de transição ao alterar o tema claro e escuro.
    
    commit b1df01fac89679a9608e9630e40cbd0a2b143215
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Fri Oct 25 07:55:30 2024 -0300
    
        chore(theme): adição dos comandos para alterar o valor da variável CSS --letra-oposta.
    
    commit 5f532d230ec619313bcdb3dbd6a27defb87df420
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Thu Oct 24 11:18:13 2024 -0300
    
        feat: alterar ícone no botão de tema ao alterar o tema claro/escuro.
    
    commit 4bce12a7822c2c44014cd5560732a6212852fb01
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Thu Oct 24 11:08:27 2024 -0300
    
        feat: tema do sistema aplicado automaticamente ao abrir a página.
    
    commit 628239337bba8574319044c6b5791cbf7d164291
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Thu Oct 24 11:06:14 2024 -0300
    
        chore: uso de argumento padrão na função aplicarTema().
    
    commit c704861036d15366971e81b6ae1b8bcde4908970
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Thu Oct 24 11:00:03 2024 -0300
    
        feat: função alterarTema() chamar a função aplicarTema().
    
    commit d659836d4591f7e9d4def50e947495c197b28daa
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Thu Oct 24 10:58:40 2024 -0300
    
        feat: função aplicarTema() que altera os valores das variáveis.
    
    commit 2a39837841fc77e38332a977f36bb7903e0ec29b
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Thu Oct 24 10:39:58 2024 -0300
    
        feat: script da função que altera o tema entre "light" ou "dark".
    
    commit 21766470e89f95860eb9dcd9839ce082782c508a
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Thu Oct 24 10:33:51 2024 -0300
    
        feat: função que obtêm o tema atual do navegador do usuário.
    
    commit a9aa9e56d98b5da428d0cf8457d3758008f04b90
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Thu Oct 24 10:29:28 2024 -0300
    
        chore: criação do elemento DOM de botão tema, adição do evento listener e teste usando a função alterarTema().
    
    commit 5e765693a8e1df0c613517e552baed0cc743aa87
    Author: Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
    Date:   Thu Oct 24 10:27:32 2024 -0300
    
        feat: criação do script de alteração do tema claro e escuro.

---
+++ frontend/javascripts/interface/tema.js
@@ -0,0 +1,107 @@
+"use strict"
+
+/*
+ * Este script é responsável por alterar o tema do site entre claro e escuro.
+ */
+
+// Obter tema atual do sistema
+let temaAtual = getTemaAtual()
+// Aplicar o tema atual
+aplicarTema(temaAtual)
+
+// Obter botão de menu pelo DOM
+const botaoTema = document.getElementById("botao-tema")
+// Adicionar evento listener ao clicar no botão de tema
+botaoTema.addEventListener("click", alterarTema)
+
+/**
+ * Retorna o tema atual do sistema do usuário.
+ * A função pode retornar os valores em strings "light" ou "dark".
+ */
+function getTemaAtual() {
+  // Condicional para verificar tema do navegador cliente
+  if (window.matchMedia("(prefers-color-scheme: light").matches) {
+    // Tema claro
+    return "light"
+  } else {
+    // Tema escuro
+    return "dark"
+  }
+}
+
+/**
+ * Altera o valor do tema atual (light ou dark).
+ */
+function alterarTema() {
+  // Alterar valor da variável
+  temaAtual = temaAtual == "light" ? "dark" : "light"
+
+  // Aplicar o tema
+  aplicarTema(temaAtual)
+}
+
+/**
+ * Aplica o tema (claro ou escuro), alterando as variáveis CSS de cores e o botão de tema.
+ *
+ * @param {string} theme O tema definido pelo navegador ou alterado pelo usuário ("light" ou "dark"). Por padrão, é tema claro ("light").
+ */
+function aplicarTema(
+  theme = "light" // Tema a ser aplicado (padrão: claro)
+) {
+  // Obter elemento raiz (:root)
+  const root = document.documentElement
+
+  // Obter ícones dos botões de tema
+  const lightIcon = document.getElementById("icone-light-mode")
+  const darkIcon = document.getElementById("icone-dark-mode")
+
+  // Obter e adicionar classe ao body
+  const body = document.querySelector("body")
+  body.classList.add("alterar-tema")
+
+  if (theme == "light") {
+    /* Aplicar tema claro */
+
+    // Background
+    root.style.setProperty("--background", "var(--background-claro)")
+    root.style.setProperty("--background-contraste", "var(--background-escuro)")
+
+    // Background transparentes
+    root.style.setProperty("--background-transparente", "var(--background-claro-transparente)")
+
+    // Cor da letra
+    root.style.setProperty("--letra-uso", "var(--letra-preta)")
+    root.style.setProperty("--letra-oposta", "var(--letra-branca)")
+
+    // Ocultar botão de modo claro
+    lightIcon.style.display = "none"
+    // Exibir botão de modo escuro
+    darkIcon.style.display = "block"
+  } else {
+    /* Aplicar tema escuro */
+
+    // Backgrounds
+    root.style.setProperty("--background", "var(--background-escuro)")
+    root.style.setProperty("--background-contraste", "var(--background-claro)")
+
+    // Background transparentes
+    root.style.setProperty("--background-transparente", "var(--background-escuro-transparente)")
+
+    // Cor da letra
+    root.style.setProperty("--letra-uso", "var(--letra-branca)")
+    root.style.setProperty("--letra-oposta", "var(--letra-preta)")
+
+    // Exibir botão de modo claro
+    lightIcon.style.display = "block"
+    // Ocultar botão de modo escuro
+    darkIcon.style.display = "none"
+  }
+
+  // Remover classe de transição de tema depois de um tempo
+  setTimeout(
+    () => {
+      body.classList.remove("alterar-tema")
+    },
+    550 // 0.55 segundos
+  )
+}
