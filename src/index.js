5a161a1402a12725276f650c49ad4ea0e925f923
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2024-12-02 08:16:14 -0300

    feat(backend): iniciação do projeto NodeJS

---
+++ src/index.js
@@ -0,0 +1,33 @@
+"use strict"
+
+/* Importação de dependências */
+import express, { json } from "express" // Express: criar o servidor | json: processar requisições JSON
+import cors from "cors" // CORS: permite requisições de diferentes origens
+
+/** Instância do aplicativo Express */
+const app = express()
+/* Middlewares */
+app.use(json()) // processar requisições com corpo em JSOn
+app.use(cors) // permitir requisições de outras origens
+
+/*
+ * Configurar o servidor para escutar a porta 3000.
+ */
+app.listen(
+  3000, // porta
+  () => {
+    /* callback */
+    try {
+      /* código executado quando o servidor inicia */
+
+      // Mensagem de tudo OK
+      console.log(`\n${"-".repeat(75)}
+      Servidor em execução.`)
+    } catch (e) {
+      /* tratamento de erros */
+      console.log(`\n${"-".repeat(75)}
+      Houve um erro na conexão:\n
+      ${e}\n`)
+    }
+  }
+)
